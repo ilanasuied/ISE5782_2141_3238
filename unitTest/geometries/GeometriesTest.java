756bda268653fb593c3cc6ec7e543f12c5506e4a
sivane guedj <sivane@g.jct.ac.il>
2022-03-23 12:04:55 +0200

    test for collection

--- unitTest/geometries/GeometriesTest.java
+++ unitTest/geometries/GeometriesTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
  * testing collection of geometries
  */
 class GeometriesTest {
-    Sphere sph = new Sphere(new Point(0,0,1),1);
+    Sphere sph = new Sphere(new Point(1,0,0),1);
     Plane pl = new Plane(new Point(0,0,1),new Point(0,1,0),new Point(1,0,0));
     Triangle tr = new Triangle(new Point(0,0,1),new Point(0,1,0),new Point(2,0,0));
     Geometries collection = new Geometries(sph,pl,tr);
@@ -31,18 +31,27 @@ class GeometriesTest {
     void testFindIntersections() {
         // ================================================= Equivalence Partitions Tests ===================================================
         //TC01:some Geometries are intersected (not all of them)
-        Ray ray= new Ray(new Point(0,0,0.5),new Vector(3,0,0));
+        Ray ray= new Ray(new Point(0,-1,1),new Vector(3,2,-0.5));
         assertEquals(3,collection.findIntersections(ray).size(),"Wrong number of intersections");
+        //only plane and sphere are intersected
 
 
         // ================================================= Boundary Values Tests ==========================================================
         //TC11: All the Geometries are intersected
+        ray= new Ray(new Point(0,-0.5,0.5),new Vector(3,2.5,0));
+        assertEquals(4,collection.findIntersections(ray).size(),"Wrong number of intersections");
 
         //TC12: No Geometries are intersected
+        ray= new Ray(new Point(0,-0.5,0.5),new Vector(-2,-1.5,0.5));
+        assertNull(collection.findIntersections(ray),"There are no intersections");
 
         //TC13: Only one Geometrie is intersected
+        ray= new Ray(new Point(0,-0.5,0.5),new Vector(0,0.5,2.5));
+        assertEquals(1,collection.findIntersections(ray).size(),"Wrong number of intersections");
 
         //TC14: Empty Geometries collection
+        collection = new Geometries();
+        assertNull(collection.findIntersections(ray),"There are no Geometries");
     }
     
 }
