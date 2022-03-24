614380b97d782f6b4a06d4a08a92ea153b50345b
ilana suied <ilanasuied@gmail.com>
2022-03-24 17:20:47 +0200

    targil 4 camera test

--- src/renderer/Camera.java
+++ src/renderer/Camera.java
@@ -4,8 +4,10 @@ import primitives.Point;
 import primitives.Ray;
 import primitives.Vector;
 
+import static primitives.Util.isZero;
+
 public class Camera {
-    //revoir les valeurs
+
 
     //point's location of the VP
     private Point location;
@@ -26,117 +28,54 @@ public class Camera {
     //camera's distance
     private double _distance=10;
 
-                                 //================================== constructors =========================
 
-    public Camera(Point location,Vector Vup,Vector Vto) {
-
-        if (Vup.dotProduct(Vto)!=0)
-            throw new IllegalArgumentException("The direction vectors must be perpendicular to each other");
-        this.location = location;
-        this.Vto=Vto;
-        this.Vup=Vup;
-        this.Vright=
-        this.Vto.normalize();
-        this.Vup.normalize();
-        this.Vright.normalize();
-    }
-
-
-    //================================== getters =========================
 
     /**
-     * @return the location of the VP
+     * Builder Class for Camera
      */
-    public Point getLocation() {
-        return location;
-    }
+    public static class BuilderCamera {
+        final private Point _p0;
+        final private Vector _vTo;
+        final private Vector _vUp;
+        final private Vector _vRight;
 
-    /**
-     * return the vector that representing forward direction
-     * @return vector Vto
-     */
-    public Vector getVto() {
-        return Vto;
-    }
+        private double _distance = 10;
+        private double _width = 1;
+        private double _height = 1;
 
-    /**
-     * return the vector that representing up direction
-     * @return vector Vup
-     */
-    public Vector getVup() {
-        return Vup;
-    }
-
-    /**
-     * this vector is vertical to the others 2 vectors (Vup and Vto)
-     * @return vector Vright
-     */
-    public Vector getVright() {
-        return Vright;
-    }
-
-    /**
-     * return the value of the VP
-     * @return camera's height
-     */
-    public double get_height() {
-        return _height;
-    }
-
-    /**
-     * return the width of the VP
-     * @return camera's width
-     */
-    public double get_width() {
-        return _width;
-    }
-
-    /**
-     * return the distance of the VP
-     * @return camera's distance
-     */
-    public double get_distance() {
-        return _distance;
-    }
-
-                                  //================================== setters =========================
+        public BuilderCamera setDistance(double distance) {
+            _distance = distance;
+            return this;
+        }
 
 
-    public void set_height(double _height) {
-        this._height = _height;
-    }
+        public BuilderCamera setViewPlaneWidth(double width) {
+            _width = width;
+            return this;
+        }
 
-    public void set_width(double _width) {
-        this._width = _width;
-    }
+        public BuilderCamera setViewPlaneHeight(double height) {
+            _height = height;
+            return this;
+        }
 
-    public void set_distance(double _distance) {
-        this._distance = _distance;
-    }
+        public Camera build() {
+            Camera camera = new Camera(this);
+            return camera;
+        }
 
+        public BuilderCamera(Point p0, Vector vTo, Vector vUp) {
+            _p0 = p0;
 
+            if (!isZero(vTo.dotProduct(vUp))) {
+                throw new IllegalArgumentException("vto and vup are not orthogonal");
+            }
 
+            _vTo = vTo.normalize();
+            _vUp = vUp.normalize();
 
-    public Camera setVPSize(double width, double height){
+            _vRight = _vTo.crossProduct(_vUp);
 
-
-
-
-
-        return this;
-    }
-
-    public Camera setVPDistance(double distance){
-
-
-
-
-        return this;
-    }
-
-    public Ray constructRay(int nX, int nY, int j, int i){
-
-        return null;
+        }
     }
 }
-
