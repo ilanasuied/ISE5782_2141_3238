1d2a9b51556a02b0a467c66f615615a2e71faf84
sivane guedj <sivane@g.jct.ac.il>
2022-05-01 16:23:22 +0300

    targil 6 - directional light

--- src/lighting/DirectionalLight.java
+++ src/lighting/DirectionalLight.java
@@ -4,22 +4,36 @@ import primitives.Color;
 import primitives.Point;
 import primitives.Vector;
 
-public class DirectionalLight extends Light {
+public class DirectionalLight extends Light implements LightSource {
 
     private Vector direction;
 
+    /**
+     * primary constructor for directional light
+     * @param _intensity value for the intensity of the light
+     * @param _direction value for the diection of the light
+     */
+    public DirectionalLight(Color _intensity, Vector _direction) {
+        super(_intensity);
+        this.direction = _direction.normalize();
+    }
 
     @Override
     /**
-     *
+     * getter function for intensity
      */
     public Color getIntesity(Point p){
         return super.getIntensity();
+        //new Color(getIntensity);
     }
 
+    @Override
+    /**
+     * getter function for getL
+     */
     public Vector getL(Point p)
     {
-        return super.getL();
+        return new Vector(direction);
     }
 
 }
