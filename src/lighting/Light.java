209032a5a023dbf8a085062d04d70e417b278c65
sivane guedj <sivane@g.jct.ac.il>
2022-05-02 13:27:00 +0300

    comments lights and material

--- src/lighting/Light.java
+++ src/lighting/Light.java
@@ -5,10 +5,18 @@ import primitives.Color;
 abstract class Light {
     private Color intensity;
 
+    /**
+     * constructor for intensity
+     * @param intensity
+     */
     protected Light(Color intensity) {
         this.intensity = intensity;
     }
 
+    /**
+     * getter for intensity
+     * @return intensity
+     */
     public Color getIntensity() {
         return intensity;
     }
--- src/primitives/Material.java
+++ src/primitives/Material.java
@@ -5,11 +5,21 @@ public class Material {
     private double ks = 0d;
     private int shininess = 0;
 
+    /**
+     * set diffuse factor/coefficient with double type parameter
+     * @param kd
+     * @return material kd
+     */
     public Material setKd(double kd) {
         this.kd = kd;
         return this;
     }
 
+    /**
+     * set diffuse factor/coefficient with Double3 type parameter
+     * @param kd
+     * @return
+     */
     public Material setKd(Double3 kd) {
         kd.scale(this.kd);
         return this;
@@ -24,24 +34,49 @@ public class Material {
     }
 
 
+    /**
+     * set specular factor/coefficient with double type parameter
+     * @param ks
+     * @return material ks
+     */
     public Material setKs(double ks) {
         this.ks = ks;
         return this;
     }
+
+    /**
+     * set diffuse factor/coefficient with Double3 type parameter
+     * @param ks
+     * @return material ks
+     */
     public Material setKs(Double3 ks) {
         ks.scale(this.ks);
         return this;
     }
 
+
+    /**
+     * getter for ks specular factor
+     * @return
+     */
     public double getKs() {
         return ks;
     }
 
+    /**
+     * setter for shininess value
+     * @param shininess
+     * @return material shininess
+     */
     public Material setShininess(int shininess) {
         this.shininess = shininess;
         return  this;
     }
 
+    /**
+     * getter for shininess value
+     * @return
+     */
     public int getShininess() {
         return shininess;
     }
